8a4ae8691b8e56610c06e0c197274f91882df264
kreativjamesz <kreativjames@gmail.com>
2018-12-15 13:01:08 +0800

    Updates

---
+++ precache-manifest.794f031d5e91d462b753a486abdcec35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/components-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fcc1d8dfeec6f9774775",
+    "url": "/components-demo/static/js/runtime~main.fcc1d8df.js"
+  },
+  {
+    "revision": "78fd2f4cef7d8cac83ad",
+    "url": "/components-demo/static/js/main.78fd2f4c.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/components-demo/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "78fd2f4cef7d8cac83ad",
+    "url": "/components-demo/static/css/main.1c466d32.chunk.css"
+  },
+  {
+    "revision": "9572ad88134da3701927518e1735df3b",
+    "url": "/components-demo/index.html"
+  }
+];
